84679f7db799802e08bd874c9ce3ab62c5b41d2a
DESKTOP-0C3L2P9\DEV <wwww3426@naver.com>
2018-03-26 16:46:53 +0900

    111

--- javascripts/worker-test.js
+++ javascripts/worker-test.js
@@ -12,11 +12,11 @@ if (window.Worker){  // 브라우져에서 워커를 지원 여부 확인
     } else if (browserInfo == 'ie11') {
         alert(browserInfo); 
         // Util.initWebWoker();          //  IE10 11.0.9600.188860 에서 돌아감 
-        // Util.initWorkerFallback();    //  IE 11 동작 여부 확인함 
+        Util.initWorkerFallback();    //  IE 11 동작 여부 확인함 
         // Util.initWorkerFallback2();   //  IE 11 동작 여부 확인함 
     } else {
         // Util.initWebWoker();          //  크롬,엣지 동작 확인
-        // Util.initWorkerFallback();   
+        Util.initWorkerFallback();   
         // Util.initWorkerFallback2();   
     }
 
